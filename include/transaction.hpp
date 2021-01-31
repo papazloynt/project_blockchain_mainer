f451ed4f52fb6892b9d7a1ecb832aecabc0834fd
papazloynt <cool.chastikov53@gmail.com>
2021-01-31 16:07:04 +0300

    Added PicoSHA2

--- include/transaction.hpp
+++ include/transaction.hpp
@@ -3,16 +3,20 @@
 #ifndef INCLUDE_SUGGESTIONS_HPP_
 #define INCLUDE_SUGGESTIONS_HPP_
 
-#include <grpcpp/grpcpp.h>
+// private headers
+#include "blockhain.hpp"
 
+#include <grpcpp/grpcpp.h>
 /*#include <google/protobuf/repeated_field.h>*/
 #include <blockchain.grpc.pb.h>
 #include <shared_mutex>
-#include <nlohmann/json.hpp>
 #include <string>
 #include <fstream>
 #include<thread>
+// SQLite3
 #include <sqlite3.h>
+// picosha
+#include <picosha2.h>
 
 
 struct Transac {
@@ -26,10 +30,40 @@ struct Transac {
                             sum(stoi(request->req().client_from())) {}
 };
 
+void CreateDataBase(sqlite3* db,char* err){
+  //Создаём таблицу, имя - единственное, повторяться не может
+  int rc = sqlite3_exec(db, "CREATE TABLE IF NOT EXISTS information("
+                            "name TEXT NOT NULL,"
+                            "password TEXT NOT NULL,"
+                            "sum INT,"
+                            "PRIMARY KEY (name));",
+                        NULL, NULL, &err);
+  if ( rc != SQLITE_OK) {
+    std::cout << "error:" << err << std::endl;
+    exit(1);
+  }
+  else {
+    std::cout << "Table created successfully" << std::endl;
+  }
+}
+
+void InsertPerson(sqlite3* db, char* err) {
+  std::string name;
+  std::string password;
+}
+
 class Mainer : public blockchain::Blockchain::Service {
  public:
-  Mainer();
+  Mainer() : db(nullptr), err(nullptr) {
+    sqlite3_open("Data.db", &db);
+    CreateDataBase(db,err);
+
+
+  }
+  sqlite3* db;
+  char* err;
  private:
+  std::shared_mutex sh_mutex;
    grpc::Status Transaction(grpc::ServerContext* context,
                       const blockchain::TransactionRequest* request,
                       blockchain::TransactionResponse* response) override {
