d92ff63ba933beb08d75b75adfeade62ccf4a746
papazloynt <cool.chastikov53@gmail.com>
2021-02-01 04:46:49 +0300

    Change vector on list, symantycs

--- include/blockhain.hpp
+++ include/blockhain.hpp
@@ -5,24 +5,43 @@
 #ifndef BLOCKCHAIN_BLOCKHAIN_HPP
 #define BLOCKCHAIN_BLOCKHAIN_HPP
 
-#include <vector>
+// c++ header
+#include <list>
 #include <iterator>
+#include <string>
 
-class Block {
+// PicoSHA2
+#include <picosha2.h>
+
+
+
+struct Transac {
+  std::string client_from;
+  std::string client_to ;
+  uint32_t sum;
+  Transac() : client_from(), client_to(), sum(0) {}
+  explicit Transac(const std::string c_f, const std::string c_t, uint32_t s) :
+      client_from(c_f),
+      client_to(c_t),
+      sum(s) {}
+
+};
+
+struct Block {
+  unsigned position;
+  std::string block_hash;
   std::string transaction_hash;
-  Block* prev_ptr;
+  std::string prev_block_hash;
 
-  Block(std::string transaction_hash_, Block* ptr) :
-              transaction_hash(transaction_hash_),
-               prev_ptr(ptr) {}
+  Block(unsigned i, std::string b_h, std::string t_h, std::string p_h) :
+                        position(i), block_hash(b_h),
+                        transaction_hash(t_h), prev_block_hash(p_h) {}
 };
 
 
-class BlockChain {
- private:
-  std::vector <Block> block_chain;
+struct BlockChain {
+  std::list <Block> block_chain;
 
- public:
   BlockChain(const BlockChain& b) : block_chain(b.block_chain) {}
 
   BlockChain& operator= (const BlockChain& b) {
@@ -32,14 +51,22 @@ class BlockChain {
     return *this;
   }
 
-  void add_block(const InfoTransaction& info){
-    //Сделать сумму по элментам info и найти хэш
-    std::string transaction_hash_ = picosha2(info);
-    if (block_chain.size != 0) {
-      block_chain.emplace_back(transaction_hash_,
-                               &block_chain(block_chain.size()-1));
+  void add_block(const Transac& info){
+    std::string transaction_hash =
+        picosha2::hash256_hex_string(info.client_from +
+                                     info.client_to +
+                                     std::to_string(info.sum));
+    std::string block_hash =
+        picosha2::hash256_hex_string(std::to_string(block_chain.size() + 1) +
+                                     transaction_hash);
+    if (block_chain.size() != 0) {
+      std::list<Block>::iterator it = block_chain.end();
+      block_chain.emplace_back(block_chain.size() + 1,
+                               block_hash,
+                               transaction_hash,
+                               (--it)->block_hash);
     } else {
-      block_chain.push_back(transaction_hash_, nullptr);
+      block_chain.emplace_back(0, block_hash, transaction_hash, "");
     }
   }
 };
